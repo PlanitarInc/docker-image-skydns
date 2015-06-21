2c1061368736f766285ae2f0050eb54fd26bc372
korya <korya@github.com>
2015-06-21 12:43:03 -0400

    Docker images: skydns: use planitar/etcd image in tests

--- Makefile
+++ Makefile
@@ -16,46 +16,12 @@ clean:
 	rm -rf ./bin ./logs
 	docker rmi -f ${IMAGE_NAME} || true
 
-test: bin/etcd bin/etcdctl
-	mkdir -p logs
-	DOCKER_IP=$$(docker run --net host --rm planitar/dev-base \
-	  bash -c "ifconfig docker0" | \
-	  sed -n "s/^.*\<inet addr:\([0-9.]\+\).*$$/\1/p"); \
-	docker run -d --name test-etcd -v `pwd`/bin:/in \
-	  -p $${DOCKER_IP}:14001:14001 planitar/dev-base \
-	  /in/etcd -addr $${DOCKER_IP}:14001 -bind-addr 0.0.0.0; \
-	docker run -d --name test-skydns -p $${DOCKER_IP}:1053:53/udp \
-	  ${IMAGE_NAME} -addr 0.0.0.0:53 -nameservers 8.8.8.8:53 \
-	  -machines http://$${DOCKER_IP}:14001 -domain test.planitar. -verbose; \
-	sleep 1s; \
-	docker run --rm -v `pwd`:/in -v `pwd`/logs:/logs \
-	  -e DOCKER_IP=$${DOCKER_IP} planitar/dev-base /in/test.sh \
-	  >logs/logs.test.txt 2>&1; \
-	res=$$?; \
-	if [ $$res -ne 0 ]; then \
-	  docker logs test-etcd > logs/logs.etcd.txt; \
-	  docker logs test-skydns > logs/logs.skydns.txt; \
-	  echo "    Logs are at `pwd`/logs/logs.etcd.txt `pwd`/logs/logs.skydns.txt `pwd`/logs/logs.test.txt"; \
-	  echo ""; \
-	  tail logs/logs.test.txt; \
-	  docker rm -f test-etcd test-skydns; \
-	  false; \
-	fi
-	docker rm -f test-etcd test-skydns
-	rm -rf logs
+test:
+	./test.sh
 
 bin/skydns:
 	mkdir -p bin
 	docker run --rm -v `pwd`/bin:/out planitar/dev-go /bin/bash -lc ' \
 	  pkg="github.com/skynetservices/skydns" && \
 	  gobldcp "$$pkg" skydns /out \
-	'
-
-bin/etcd bin/etcdctl:
-	mkdir -p bin
-	docker run --rm -v `pwd`/bin:/out planitar/dev-go /bin/bash -lc ' \
-	  wget https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz && \
-	  tar xzvf etcd-v0.4.6-linux-amd64.tar.gz && \
-	  cp etcd-v0.4.6-linux-amd64/etcd /out && \
-	  cp etcd-v0.4.6-linux-amd64/etcdctl /out \
 	'
---
+++ test-cases.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+set -e
+
+export PATH=/in/bin:$PATH
+
+etcdctl() {
+  case "$1" in
+    set)
+      curl -sL "http://${ETCD_ADDR}/v2/keys${2}" -XPUT -d value="$3"
+      ;;
+    get)
+      curl -sL "http://${ETCD_ADDR}/v2/keys${2}"
+      ;;
+    *)
+      echo "etcdctl $1 is not implemeneted" >&2
+      return 1
+  esac
+}
+
+# Set the nameserver
+etcdctl set /skydns/planitar/test/dns/ns \
+  '{"host":"127.0.0.1", "priority":10, "weight":10}'
+dig @${DOCKER_IP} -p1053 NS test.planitar | grep ^test.planitar. | \
+  grep -w NS | grep -q ns.dns.test.planitar.
+dig @${DOCKER_IP} -p1053 NS test.planitar | grep ^ns.dns.test.planitar. | \
+  grep -w A | grep -q 127.0.0.1
+
+#
+etcdctl set /skydns/planitar/test/a/01 \
+  '{"host":"planitar.com", "port":9001, "priority":10, "weight":30}'
+etcdctl set /skydns/planitar/test/a/02 \
+  '{"host":"example.com", "port":9002, "priority":10, "weight":10}'
+etcdctl set /skydns/planitar/test/b/01 \
+  '{"host":"10.0.2.3", "port":9003, "priority":20}'
+
+dig @${DOCKER_IP} -p1053 SRV 01.a.test.planitar | \
+  grep -q '^01.a.test.planitar\..*.\<SRV\>.*\<10\>.*\<100\>.*\<9001\>.*planitar.com'
+dig @${DOCKER_IP} -p1053 SRV 01.a.test.planitar | \
+  grep -q '^planitar.com\..*.\<A\>'
+dig @${DOCKER_IP} -p1053 SRV 02.a.test.planitar | \
+  grep -q '^02.a.test.planitar\..*\<SRV\>.*\<10\>.*\<100\>.*\<9002\>.*\<example.com'
+dig @${DOCKER_IP} -p1053 SRV 02.a.test.planitar | \
+  grep -q '^example.com\..*.\<A\>'
+dig @${DOCKER_IP} -p1053 SRV 01.b.test.planitar | \
+  grep '^01.b.test.planitar\..*\<SRV\>.*\<20\>.*\<100\>.*\<9003\>.*\<01.b.test.planitar\>'
+dig @${DOCKER_IP} -p1053 SRV 01.b.test.planitar | \
+  grep -q '^01.b.test.planitar\..*.\<A\>.*\<10.0.2.3\>'
+
+dig @${DOCKER_IP} -p1053 SRV a.test.planitar | \
+  grep -q '^a.test.planitar\..*.\<SRV\>.*\<10\>.*\<75\>.*\<9001\>.*planitar.com'
+dig @${DOCKER_IP} -p1053 SRV a.test.planitar | \
+  grep -q '^a.test.planitar\..*.\<SRV\>.*\<10\>.*\<25\>.*\<9002\>.*example.com'
+
+dig @${DOCKER_IP} -p1053 SRV test.planitar | \
+  grep -q '^test.planitar\..*.\<SRV\>.*\<10\>.*\<60\>.*\<9001\>.*planitar.com'
+dig @${DOCKER_IP} -p1053 SRV test.planitar | \
+  grep -q '^test.planitar\..*.\<SRV\>.*\<10\>.*\<20\>.*\<9002\>.*example.com'
+dig @${DOCKER_IP} -p1053 SRV test.planitar | \
+  grep -q '^test.planitar\..*.\<SRV\>.*\<20\>.*\<100\>.*\<9003\>.*01.b.test.planitar'
